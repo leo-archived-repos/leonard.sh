d3f2714ec972f8014be8ce7d603facaf21e11dba
Narcis B <leonardssh22@gmail.com>
2022-07-09 21:15:36 +0200

    chore: initial files

---
+++ app/root.tsx
@@ -0,0 +1,52 @@
+import styles from "./tailwind.css";
+
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "soonTM",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap",
+    },
+    { rel: "stylesheet", href: styles },
+  ];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="absolute top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2">
+      <h1 className="font-sans text-4xl text-black/60">Be right back.</h1>
+    </div>
+  );
+}
